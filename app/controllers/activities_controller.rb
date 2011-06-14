19f9d2c85238f2e38f33502fa61141b8071ab4de
MarkBrough <mark.brough@gmail.com>
2011-06-14 15:24:17 +0100

    modified:   app/controllers/activities_controller.rb 	modified:   log/development.log

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   # GET /activities
   # GET /activities.xml
   def index
-    @numcountries = Activity.all(:id, :group=>:recipient_country).count
+    @numcountries = Activity.count(:recipient_country, :distinct=>true)
     @limit = 10.to_f
     if params[:page]
       @page = params[:page]
