5773924c99f22d8d2f589b29055f82eafb0e2fb3
MarkBrough <mark.brough@gmail.com>
2011-06-14 19:45:07 +0100

    modified:   app/controllers/activities_controller.rb 	modified:   log/development.log

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -14,7 +14,7 @@ class ActivitiesController < ApplicationController
     @conditions = {}
     @conditions[:hierarchy] = '1' unless !(params[:implementing_org].blank? and params[:recipient_country].blank? and params[:iati_identifier].blank? and params[:recipient_region].blank? and params[:policy_marker].blank?)
     @conditions[:implementing_org] = params[:implementing_org] unless params[:implementing_org].blank?
-    @conditions[:recipient_country] = [params[:recipient_country].titleize, params[:recipient_country]] unless params[:recipient_country].blank?
+    @conditions[:recipient_country] = [params[:recipient_country].titleize, params[:recipient_country].capitalize] unless params[:recipient_country].blank?
     @conditions[:recipient_region] = params[:recipient_region] unless params[:recipient_region].blank?
     @conditions[:iati_identifier] = params[:iati_identifier] unless params[:iati_identifier].blank?
     if @conditions[:recipient_country]
