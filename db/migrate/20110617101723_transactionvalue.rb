e4316e9d78a8195f7c268d21df44a2e4ecd3dd5c
MarkBrough <mark.brough@gmail.com>
2011-06-17 12:26:46 +0100

    modified:   db/migrate/20110617101723_transactionvalue.rb

--- db/migrate/20110617101723_transactionvalue.rb
+++ db/migrate/20110617101723_transactionvalue.rb
@@ -1,7 +1,14 @@
 class Transactionvalue < ActiveRecord::Migration
   def self.up
-	change_table :transactions do |t|
-  	  t.change :value, :float
+	case ActiveRecord::Base.connection.adapter_name
+	when 'SQLite'
+		change_table :transactions do |t|
+	  	  t.change :value, :integer
+		end
+	when 'PostgreSQL'
+	     execute %{ALTER TABLE "transactions" ALTER COLUMN value TYPE integer USING CAST(CASE value WHEN '' THEN NULL ELSE value END AS INTEGER)}
+	else
+	     raise 'Migration not implemented for this DB adapter'
 	end
   end
 
