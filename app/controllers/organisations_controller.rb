ba0036c5c8804cc06f1f1ff995415f34e54b4e48
MarkBrough <mark.brough@gmail.com>
2011-06-24 02:14:34 +0200

    modified:   app/controllers/organisations_controller.rb 	modified:   app/views/organisations/index.html.erb 	modified:   log/development.log

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,6 +1,34 @@
 class OrganisationsController < ApplicationController
 	def index
-	    @organisations = Organisation.find(:all, :order=>"name")
+	   @limit = 200.to_f
+	    if params[:page]
+	      @page = params[:page]
+	      @page = @page.to_f
+	      @page = @page -1
+	    else
+	      @page = 0
+	    end
+	    pagemultiplier = @limit * @page
+	    @organisations = Organisation.find(:all, :order=>"name", :limit=>@limit, :offset=>pagemultiplier)
+
+
+	    # get total number of rows
+	    @totalrows = Organisation.count.to_f
+	    @numpages = (@totalrows / @limit)
+	    @pagination = []
+		for i in 1..@numpages
+		  @pagination << {
+			:page => i.to_s
+			}
+		end
+	
+		if ((@numpages.to_f - i.to_f) > 0) and i
+			@pagination << {
+				:page => (i+1).to_s
+			}
+		end
+
+
 
 	    respond_to do |format|
 	      format.html # index.html.erb
